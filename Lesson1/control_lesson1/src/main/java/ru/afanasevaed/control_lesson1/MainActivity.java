160b585ac64ad97d10dfd1b8acd839f426177203
Afanaseva ED <yabluchniisik@mail.ru>
2026-02-25 11:30:27 +0300

    Первая практика

---
+++ Lesson1/control_lesson1/src/main/java/ru/afanasevaed/control_lesson1/MainActivity.java
@@ -0,0 +1,37 @@
+package ru.afanasevaed.control_lesson1;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+        // ГЛАВА 6
+        TextView myTextView = findViewById(R.id.textView3);
+        myTextView.setText("New text in MIREA");
+        Button button = findViewById(R.id.button2);
+        button.setText("MireaButton");
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        checkBox.setChecked(true);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
